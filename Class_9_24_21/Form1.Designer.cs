1589945fbda68aaa1e81c438c48561c9cc02981e
uneronnik <NIKI.FOMENKO.7@GMAIL.COM>
2021-09-24 23:22:40 +0300

    1.0

--- Class_9_24_21/Form1.Designer.cs
+++ Class_9_24_21/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.ExpressionLine.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.ExpressionLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ExpressionLine.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(84)))));
-            this.ExpressionLine.Location = new System.Drawing.Point(9, 17);
+            this.ExpressionLine.Location = new System.Drawing.Point(10, 15);
             this.ExpressionLine.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.ExpressionLine.Name = "ExpressionLine";
             this.ExpressionLine.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
@@ -54,7 +54,7 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
-            this.button1.Location = new System.Drawing.Point(9, 48);
+            this.button1.Location = new System.Drawing.Point(10, 49);
             this.button1.Margin = new System.Windows.Forms.Padding(0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(164, 56);
@@ -67,15 +67,19 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(158)))), ((int)(((byte)(173)))));
             this.ClientSize = new System.Drawing.Size(183, 114);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.ExpressionLine);
             this.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.Text = "Calculator";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
