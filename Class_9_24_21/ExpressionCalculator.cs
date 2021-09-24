f7a02914158d0b376d9ca76abda0c4a5e10a1311
uneronnik <NIKI.FOMENKO.7@GMAIL.COM>
2021-09-24 21:51:21 +0300

    1.0

---
+++ Class_9_24_21/ExpressionCalculator.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_9_24_21
+{
+
+    public class ExpressionCalculator
+    {
+        
+        public double CarculateEpression(string expression)
+        {
+            Stack<char> operationsStack = new Stack<char>();
+            Stack<double> numsStack = new Stack<double>();
+            bool isNextNegative = false;
+            foreach (char temp in expression)
+            {
+                if (temp == '/')
+                    operationsStack.Push(temp);
+                if (temp == '*')
+                    operationsStack.Push(temp);
+                if (temp == '+')
+                    operationsStack.Push(temp);
+                if (temp == '-')
+                {
+                    isNextNegative = true;
+                    if(numsStack.Count - operationsStack.Count != 0)
+                        operationsStack.Push('+');
+                }
+
+                if (temp >= '0' && temp <= '9')
+                {
+                    if (numsStack.Count == operationsStack.Count)
+                    {
+                        if (isNextNegative == true)
+                        {
+                            numsStack.Push(-(Convert.ToInt32(temp) - '0'));
+                            isNextNegative = false;
+                        }
+                        else
+                            numsStack.Push(Convert.ToInt32(temp) - '0');
+                    }
+                    else
+                    {
+                        double num = numsStack.Pop();
+                        num *= 10;
+                        if (num >= 0)
+                            num += Convert.ToInt32(temp) - '0';
+                        else
+                            num -= Convert.ToInt32(temp) - '0';
+                        numsStack.Push(num);
+                    }
+                }
+                
+            }
+            while (numsStack.Count != 1)
+            {
+                double bufNum, bufNum1;
+                bufNum = numsStack.Pop();
+                bufNum1 = numsStack.Pop();
+
+                char bufSymbol = operationsStack.Pop();
+
+                if (bufSymbol == '/')
+                    numsStack.Push(bufNum1 / bufNum);
+                if (bufSymbol == '*')
+                    numsStack.Push(bufNum * bufNum1);
+                if (bufSymbol == '+')
+                {
+                    if(operationsStack.Count == 0)
+                    {
+                        numsStack.Push(bufNum + bufNum1);
+                        break;
+                    }
+                    char bufPeek = operationsStack.Peek();
+                    if (bufPeek == '+' || bufPeek == '-')
+                        numsStack.Push(bufNum + bufNum1);
+                    else
+                    {
+                        double numPeek = numsStack.Peek();
+                        if (bufPeek == '/')
+                            numsStack.Push(bufNum1 / numPeek);
+                        else
+                            numsStack.Push(bufNum1 * numPeek);
+                        char symbPop = operationsStack.Pop();
+
+                        numsStack.Push(bufNum);
+                        operationsStack.Push('+');
+                    }
+
+                }
+                
+
+            }
+            return numsStack.Pop();
+        }
+    }
+}
---
+++ Class_9_24_21/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Class_9_24_21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExpressionLine = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ExpressionLine
+            // 
+            this.ExpressionLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ExpressionLine.Location = new System.Drawing.Point(12, 12);
+            this.ExpressionLine.Name = "ExpressionLine";
+            this.ExpressionLine.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ExpressionLine.Size = new System.Drawing.Size(293, 32);
+            this.ExpressionLine.TabIndex = 0;
+            this.ExpressionLine.Text = "Enter expression";
+            this.ExpressionLine.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.ExpressionLine.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(293, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 93);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ExpressionLine);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ExpressionLine;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
